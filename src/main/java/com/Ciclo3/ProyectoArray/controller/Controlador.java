c8ac66cf735b043c66d0a6fa7aa6b96dec24f403
nelsonDeveloper41 <nelsondeveloper369@gmail.com>
2022-09-13 14:39:11 -0500

    Hasta este punto se completa todo lo necesario para Empleado (Servicios, Controladores, vistas con las plantillas Html, y adicional un filtro para ver todos los empleados por cada empresa)

--- src/main/java/com/Ciclo3/ProyectoArray/controller/Controlador.java
+++ src/main/java/com/Ciclo3/ProyectoArray/controller/Controlador.java
@@ -125,6 +125,47 @@ public class Controlador {
         return "redirect:/AgregarEmpleado";
     }
 
+    @GetMapping("/EditarEmpleado/{id}")
+    public String editarEmpleado(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        Empleado empl=empleadoService.getEmpleadoById(id).get();
+        //Creamos un atributo para el modelo, que se llame igualmente empl y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("empl",empl);
+        model.addAttribute("mensaje", mensaje);
+        List<Empresa> listaEmpresas= objempServicio.listarAllEmpresas();
+        model.addAttribute("emprelist",listaEmpresas);
+        return "editarEmpleado";
+    }
+
+    @PostMapping("/ActualizarEmpleado")
+    public String updateEmpleado(@ModelAttribute("empl") Empleado empl, RedirectAttributes redirectAttributes){
+        if(empleadoService.saveOrUpdateEmpleado(empl)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
+            return "redirect:/VerEmpleados";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/EditarEmpleado/"+empl.getId();
+
+    }
+
+    @GetMapping("/EliminarEmpleado/{id}")
+    public String eliminarEmpleado(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if (empleadoService.deleteEmpleado(id)){
+            redirectAttributes.addFlashAttribute("mensaje","deleteOK");
+            return "redirect:/VerEmpleados";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "deleteError");
+        return "redirect:/VerEmpleados";
+    }
+
+    @GetMapping("/Empresa/{id}/Empleados") //Filtrar los empleados por empresa
+    public String verEmpleadosPorEmpresa(@PathVariable("id") Integer id, Model model){
+        List<Empleado> listaEmpleados = empleadoService.obtenerPorEmpresa(id);
+        model.addAttribute("emplelist",listaEmpleados);
+        return "verEmpleados"; //Llamamos al html con el emplelist de los empleados filtrados
+    }
+
+
+
            /*
     FIN CONTROLLER EMPLEADO******************************************************
     *******************************************************************************
